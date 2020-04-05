8392422f779749b9a888520a2480a1aaee676a96
Dostaglou <dostaglou@gmail.com>
2020-04-05 16:19:50 +0900

    [Bug/Refactor](LineBot): can now query countries with a space and refactored to make it cleaner

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -47,70 +47,145 @@ class LineBotController < ApplicationController
 
 
   def text_message_builder(message)
-    if /(help)/i.match(message)
-      message_help
-    elsif /(signup)/i.match(message)
-      message_signup
-    elsif /(delete)/i.match(message)
-      message_delete
-    elsif /(turn-off)/i.match(message)
-      message_turn_off
-    elsif /\w+(-update)/i.match(message)
-      message_update_country(/\w+(-update)/i.match(message).to_s.split("-")&.first)
-    elsif /\w(-countries)/i.match(message)
-      message_x_country_list(/\w(-countries)/i.match(message).to_s.first)
-    elsif (message.gsub(/\W/, ' ').split(' ') & Country::COUNTRY_NAMES).length > 0
-      message_country_data((message.gsub(/\W/, ' ').split(' ') & Country::COUNTRY_NAMES).first)
+    # TOO LONG
+    if message.length > 50
+      message_unclear
+    # HELP
+    elsif message_help(message: message, analyze: true)
+      message_help(message: message, analyze: false)
+
+    # SIGNUP
+    elsif message_signup(message: message, analyze: true)
+      message_signup(message: message, analyze: false)
+
+    # DELETE
+    elsif message_delete(message: message, analyze: true)
+      message_delete(message: message, analyze: false)
+
+    # TURN-OFF
+    elsif message_turn_off(message: message, analyze: true)
+      message_turn_off(message: message, analyze: false)
+
+    # UPDATE (Notification)
+    elsif message_update_country(message: message, analyze: true)
+      message_update_country(message:message, analyze: false)
+
+    # LIST
+    elsif message_x_country_list(message: message, analyze: true)
+      message_x_country_list(message: message, analyze: false)
+
+    # DATA
+    elsif country_name_resolver(message: message)
+      message_country_data(message: message)
+
+    # FALLBACK
     else
       message_unclear
     end
   end
 
-  def message_help
+  def message_help(message:, analyze:)
+    if analyze
+      /(help)/i.match(message)
+    else
     "The list of options are: \n" +
     "1) Type 1 country name (capitalized) to get info on it. Ex: Japan, USA, UK. \n" +
     "2) Type LETTER-countries to get a list of those countries. Ex: j-countries. \n" +
     "3) Type Signup to get daily (9am JST) notifications about a single coutnry. \n" +
     "4) Type Turn-Off to remove daily notifications. \n" +
     "5) Type Delete to delete your account. \n"
-  end
-
-  def message_unclear
-    "I am sorry. I do not not understand. Please text help for a list of options"
-  end
-
-  def message_turn_off
-    if @user_id && user = User.find_by(line_id: @user_id)
-      user.update(line_notifications: false)
-      "Your notifications have been turned off. \n" +
-      "Thank you and stay safe!"
-    else
-      "I am sorry, you must sign up to access this feature."
     end
   end
 
-  def message_delete
-    if @user_id &&  user = User.where(line_deleted_at: nil).find_by(line_id: @user_id)
-      user.update(line_deleted_at: DateTime.now)
-      "Account deleted"
+  def message_signup(message:, analyze:)
+    if analyze
+      /(signup)/i.match(message)
     else
-      "No account found"
+      if @username && @user_id
+        user = User.find_or_initialize_by(line_id: @user_id)
+        user.attributes = { line_id: @user_id,
+                            line_name: @username,
+                            line_notifications: true,
+                            line_deleted_at: nil }
+        user.save
+        "You have been added. Please respond with COUNTRY-update. \n" +
+        "This will enable daily notifications about that country for you. \n" +
+        "You may update your choice at any time by re-texting COUNTRY-update."
+      else
+        "I am sorry, there has been an error. \n" +
+        "Please try again later or contact my master"
+      end
     end
   end
 
-  def message_x_country_list(letter)
-    countries = Country::COUNTRY_NAMES.select { |c| c.starts_with?(letter.upcase)}
-    if countries.length > 0
-      "There are #{countries.length} countries avalable starting with #{letter.upcase}. \n" +
-      "They are:  " +
-      "#{countries.join(', ')}. \n" +
-      "Please note that country names are case sensitive"
+  def message_delete(message:, analyze:)
+    if analyze
+      /(delete)/i.match(message)
     else
-      "There are no countries starting with #{letter.upcase}."
+      if @user_id &&  user = User.where(line_deleted_at: nil).find_by(line_id: @user_id)
+        user.update(line_deleted_at: DateTime.now)
+        "Account deleted"
+      else
+        "No account found"
+      end
     end
   end
 
-  def message_country_data(country)
+  def message_turn_off(message:, analyze:)
+    if analyze
+      /(turn-off)/i.match(message)
+    else
+      if @user_id && user = User.find_by(line_id: @user_id)
+        user.update(line_notifications: false)
+        "Your notifications have been turned off. \n" +
+        "Thank you and stay safe!"
+      else
+        "I am sorry, you must sign up to access this feature."
+      end
+    end
+  end
+
+  def message_update_country(message:, analyze:)
+    if analyze
+      /\w+(-update)/i.match(message)
+    else
+      if @user_id && user = User.where(line_deleted_at: nil).find_by(line_id: @user_id)
+        country = /\w+(-update)/i.match(message).to_s.split("-")&.first
+        if Country::COUNTRY_NAMES.include?(country)
+          user.update(line_countries: country)
+          "Updated. You will now be notified about #{country}"
+        else
+          "I am sorry, #{country} doesn't exist in my database. \n" +
+          message_x_country_list(letter: country.first, analyze: false, message: nil)
+        end
+      else
+        "I am sorry. \n" +
+        "You must sign up to use this feature. \n" +
+        "Text Help to learn more."
+      end
+    end
+  end
+
+  def message_x_country_list(message:, analyze:, letter: nil)
+    if analyze
+      /\w(-countries)/i.match(message)
+    else
+      letter = /\w(-countries)/i.match(message).to_s.first unless letter
+      countries = Country::COUNTRY_NAMES.select { |c| c.starts_with?(letter.upcase)}
+      if countries.length > 0
+        "There are #{countries.length} countries avalable starting with #{letter.upcase}. \n" +
+        "They are:  " +
+        "#{countries.join(', ')}. \n" +
+        "Please note that country names are case sensitive."
+      else
+        "There are no countries starting with #{letter.upcase}."
+      end
+    end
+  end
+
+  def message_country_data(message:)
+    country = country_name_resolver(message: message)
+
     entry = CovidDaily.where(country_name: country )
                       .order(created_at: :desc)
                       .limit(1)
@@ -122,37 +197,11 @@ class LineBotController < ApplicationController
     "New Cases #{entry.today_cases} - New Deaths #{entry.today_deaths}"
   end
 
-  def message_signup
-    if @username && @user_id
-      user = User.find_or_initialize_by(line_id: @user_id)
-      user.attributes = { line_id: @user_id,
-                          line_name: @username,
-                          line_notifications: true,
-                          line_deleted_at: nil }
-      user.save
-      "You have been added. Please respond with COUNTRY-update. \n" +
-      "This will enable daily notifications about that country for you. \n" +
-      "You may update your choice at any time by re-texting COUNTRY-update."
-    else
-      "I am sorry, there has been an error. \n" +
-      "Please try again later or contact my master"
-    end
-
+  def message_unclear
+    "I am sorry. I do not not understand. Please text help for a list of options"
   end
 
-  def message_update_country(country)
-    if @user_id && user = User.where(line_deleted_at: nil).find_by(line_id: @user_id)
-      if Country::COUNTRY_NAMES.include?(country)
-        user.update(line_countries: country)
-        "Updated. You will now be notified about #{country}"
-      else
-        "I am sorry, #{country} doesn't exist in my database. \n" +
-        message_x_country_list(country.first)
-      end
-    else
-      "I am sorry. \n" +
-      "You must sign up to use this feature. \n" +
-      "Text Help to learn more."
-    end
+  def country_name_resolver(message:)
+    country = Country::COUNTRY_NAMES.find { | c | message.upcase.include?(c.upcase) }
   end
 end
