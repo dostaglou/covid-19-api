e6715e20872174da519187840d1f10aa3e300a63
Dostaglou <dostaglou@gmail.com>
2020-04-11 00:13:56 +0900

    [Feature](Data): Rake to et more data + basic model methods

--- app/models/covid_daily.rb
+++ app/models/covid_daily.rb
@@ -1,3 +1,17 @@
 class CovidDaily < ApplicationRecord
   belongs_to :country
+
+  scope :most_recent, -> (x = 1) do
+                                  value = order(created_at: :desc).limit(x)
+                                  value = value.first if x == 1
+                                  value
+                                 end
+
+  def mortality_rate
+    self.total_deaths / self.total_cases.to_f * 100
+  end
+
+  def recovery_rate
+    self.recovered / self.total_cases.to_f * 100
+  end
 end
